8bc029094b76c296278094e3fdb1a2e86f1614e7
ermondel <ermondel@gmail.com>
2018-01-03 21:44:29 +0200

    Add code of enemy class

--- js/app.js
+++ js/app.js
@@ -15,10 +15,39 @@ Entity.prototype.restart = function() {
 
 
 var Enemy = function(startTileX, startTileY, finishTileX) {
+    Entity.call(this);                                               // получить свойста родительского класса
+    this.startTileX  = startTileX;                                   // номер стартового тайла по оси Х
+    this.startTileY  = startTileY;                                   // номер стартового тайла по оси Y
+    this.finishTileX = finishTileX;                                  // номер конечного тайла по оси Х
+	this.x      = engineTiles.getPxFromTileXEnemy(this.startTileX);  // установить стартовую позицию по оси X по номеру тайла
+    this.y      = engineTiles.getPxFromTileYEnemy(this.startTileY);  // установить стартовую позицию по оси Y по номеру тайла
+	this.speed  = Math.floor(Math.random() * (700 - 100)) + 100;     // установить случайную начальную скорость
+	this.sprite = 'images/enemy-bug.png';                            // установить графический тайл
 }
 Enemy.prototype = Object.create(Entity.prototype);
 Enemy.prototype.constructor = Enemy;
 Enemy.prototype.update = function(dt) {
+	this.x        = this.x + (this.speed * dt);              // сдвинуть врага + модификатор скорости * тик
+    var tileFront = engineTiles.getFrontTileXEnemy(this.x);  // посчитать передний тайл X врага
+    var tileY     = engineTiles.getFrontTileYEnemy(this.y);  // посчитать Y тайл врага
+    var tileBack  = tileFront>1 ? tileFront-1 : 0;           // посчитать задний тайл врага
+    // не отрисовывать спрайт врага если его не видно (т.е. его тайлы находяться вне игрового полотна)
+    this.visible  = (tileBack >= 6 || tileFront < 1) ? false : true;
+    // если задний тайл врага скрылся из виду, то перезапустить врага с новой скоростью
+    if (tileBack >= this.finishTileX)  {
+        this.speed = Math.floor(Math.random() * (700 - 100)) + 100;
+        this.x = engineTiles.getPxFromTileXEnemy(this.startTileX);
+    }
+    // player restart
+    // если 'передний' тайл врага совпадает с тайлом игрока - перезапустить игрока
+    if (player.playerTile == tileFront+''+tileY) {
+        player.lose();
+    }
+    // если 'зайдний' тайл врага совпадает с тайлом игрока и при этом враг 
+    // большей своей частью все еще находится в тайле - перезапустить игрока
+    if (player.playerTile == tileBack+''+tileY && !(engineTiles.moreHalfTileX(this.x))) {
+        player.lose();
+    }
 }
 
 
